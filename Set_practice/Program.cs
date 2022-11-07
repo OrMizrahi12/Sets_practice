edd2f01ec4a7d8c1fe93edb140a5291c7886d325
ormizrahi1610 <ormizrahi1610@gmail.com>
2022-11-07 09:09:38 +0200

    Add project files.

---
+++ Set_practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sets sets = new Sets(1,2,3,4,5,6,7,8,9,8,6,56,746);
+        
+
+
+        }
+    }
+}
---
+++ Set_practice/Sets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set_practice
+{
+    internal class Sets
+    {
+        bool[] onOrOff = new bool[1000];
+        private int[] setArr;
+        public Sets()
+        {
+
+        }
+
+        public Sets(params int[] intArr)
+        {
+            setArr = arrController(intArr);
+        }
+
+        int[] arrController(int[] intArr)
+        {
+            List<int> intResult = new List<int>();
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                for (int j = 0; j < onOrOff.Length; j++)
+                {
+                    if (onOrOff[i] == false && i == intArr[i])
+                    {
+                        onOrOff[i] = true;
+                        intResult.Add(i);
+                    }
+                }
+            }
+            return intResult.ToArray();
+        }
+
+        void printArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
