92dd96aedeb24510c36d46496df61c9aada76de5
ormizrahi1610 <ormizrahi1610@gmail.com>
2022-11-07 10:15:28 +0200

    update1

--- Set_practice/Program.cs
+++ Set_practice/Program.cs
@@ -10,10 +10,13 @@ namespace Set_practice
     {
         static void Main(string[] args)
         {
-            Sets sets = new Sets(1,2,3,4,5,6,7,8,9,8,6,56,746);
-        
+            Sets sets = new Sets(1,1,1,2,2,2,3,3,4,5,6,7);
+            Sets sets1 = new Sets(56, 67, 78, 89);
 
+            sets.Union(sets1);
 
+            sets1.PrintThisSetArr();
+            
         }
     }
 }
--- Set_practice/Sets.cs
+++ Set_practice/Sets.cs
@@ -8,42 +8,59 @@ namespace Set_practice
 {
     internal class Sets
     {
-        bool[] onOrOff = new bool[1000];
+        static bool[] onOrOff = new bool[1000];
         private int[] setArr;
-        public Sets()
-        {
-
-        }
+        public Sets() { }
 
         public Sets(params int[] intArr)
         {
-            setArr = arrController(intArr);
+            setArr = fixDuplicationInArr(intArr);
+            addToGlobalArr(setArr);
         }
 
-        int[] arrController(int[] intArr)
+        void addToGlobalArr(int[] intArr)
+        {
+            for (int i = 0; i < onOrOff.Length; i++)         
+                for (int j = 0; j < intArr.Length; j++)              
+                    if (!onOrOff[i] && i == intArr[j])                    
+                        onOrOff[i] = true;    
+        }
+
+        int[] fixDuplicationInArr(int[] arrInput)
+        {
+            List<int> numsResultList = new List<int>();
+
+            for (int i = 0; i < arrInput.Length; i++)
+               if(!numsResultList.Contains(arrInput[i]))
+                    numsResultList.Add(arrInput[i]);
+
+            return numsResultList.ToArray();
+        }
+
+        public static void ShowGlobalArr()
+        {
+            for (int i = 0; i < onOrOff.Length; i++)
+                if (onOrOff[i]) 
+                    Console.WriteLine(i);
+        }
+        public void PrintThisSetArr()
+        {
+            for (int i = 0; i < setArr.Length; i++)
+                Console.WriteLine(setArr[i]);         
+        }
+
+        public void Union(Sets sets)
         {
             List<int> intResult = new List<int>();
-            for (int i = 0; i < intArr.Length; i++)
-            {
-                for (int j = 0; j < onOrOff.Length; j++)
-                {
-                    if (onOrOff[i] == false && i == intArr[i])
-                    {
-                        onOrOff[i] = true;
-                        intResult.Add(i);
-                    }
-                }
-            }
-            return intResult.ToArray();
+            intResult = setArr.ToList();
+
+            for (int i = 0; i < sets.setArr.Length; i++)            
+                intResult.Add(sets.setArr[i]);
+            
+            this.setArr = intResult.ToArray();
         }
 
-        void printArr(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
-        }
+        
 
     }
 }
